ee7e198519fd0f11bf7a4d9e49d77b147a51f499
GustavoSFer <gustavo.sfernander@gmail.com>
2025-04-28 21:36:51 -0300

    feat: Iniciando o projeto

---
+++ src/main/java/br/com/fernandes/CarteiraDigitalGusApplication.java
@@ -0,0 +1,13 @@
+package br.com.fernandes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarteiraDigitalGusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarteiraDigitalGusApplication.class, args);
+	}
+
+}
