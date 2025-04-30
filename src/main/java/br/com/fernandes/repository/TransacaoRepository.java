4ab14b0f1724c348c911f89328ed0d38f8acafbe
GustavoSFer <gustavo.sfernander@gmail.com>
2025-04-29 21:46:14 -0300

    feat: Criado os repositorys das entidades

---
+++ src/main/java/br/com/fernandes/repository/TransacaoRepository.java
@@ -0,0 +1,9 @@
+package br.com.fernandes.repository;
+
+import br.com.fernandes.entities.Transacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransacaoRepository extends JpaRepository<Transacao, Long> {
+}
