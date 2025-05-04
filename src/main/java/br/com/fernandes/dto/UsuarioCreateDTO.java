5356145b5ac6b79e0256a8c595f718098e2d3e39
GustavoSFer <gustavo.sfernander@gmail.com>
2025-05-04 19:03:22 -0300

    feat: Arrumado record de usuario e comentando a parte de seguranca para a chamado do endpoint, deixando publico

---
+++ src/main/java/br/com/fernandes/dto/UsuarioCreateDTO.java
@@ -0,0 +1,4 @@
+package br.com.fernandes.dto;
+
+public record UsuarioCreateDTO() {
+}
---
+++ src/main/java/br/com/fernandes/service/interf/UsuarioInterf.java
@@ -0,0 +1,6 @@
+package br.com.fernandes.service.interf;
+
+public interface Usuario {
+
+    Usuario criarUsuario(Usuario usuario);
+}
